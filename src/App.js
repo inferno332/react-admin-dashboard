d54c7b7d11c409a03d00620bc3509ae02ad33af1
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-10-28 00:26:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSetings } from './components';
+import {
+    Ecommerce,
+    Orders,
+    Employees,
+    Customers,
+    Kanban,
+    Editor,
+    Calendar,
+    ColorPicker,
+    Line,
+    Area,
+    Bar,
+    Pie,
+    Financial,
+    ColorMapping,
+    Pyramid,
+    Stacked,
+} from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 const App = () => {
-    const activeMenu = false;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
@@ -24,9 +44,13 @@ const App = () => {
                     </div>
 
                     {activeMenu ? (
-                        <div className="sidebar w-72 fixed dark:bg-secondary-dark bg-white">Sidebar</div>
+                        <div className="sidebar w-72 fixed dark:bg-secondary-dark bg-white">
+                            <Sidebar />
+                        </div>
                     ) : (
-                        <div className="w-0 dark:bg-secondary-dark">Sidebar</div>
+                        <div className="w-0 dark:bg-secondary-dark">
+                            <Sidebar />
+                        </div>
                     )}
 
                     <div
@@ -34,18 +58,37 @@ const App = () => {
                             activeMenu ? ' md:ml-72' : 'flex-2'
                         }`}
                     >
-                        <div className="fixed md:static bg-main dark:bg-main-dark navbar w-full">Navbar</div>
+                        <div className="fixed md:static bg-main dark:bg-main-dark navbar w-full">
+                            <Navbar />
+                        </div>
                     </div>
 
                     <div>
-                      <Routes>
+                        <Routes>
                             {/* DASHBOARD */}
-                            <Route path="/" element={'Ecommerce'} />
-                            <Route path="/ecommerce" element={'Ecommerce2'} />
+                            <Route path="/" element={<Ecommerce />} />
+                            <Route path="/ecommerce" element={<Ecommerce />} />
+
                             {/* PAGES */}
-                            <Route path="/orders" element={'Order'} />
-                            <Route path="/employees" element={'Employee'} />
-                      </Routes>
+                            <Route path="/orders" element={<Orders />} />
+                            <Route path="/employees" element={<Employees />} />
+                            <Route path="/customers" element={<Customers />} />
+
+                            {/* APPS */}
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+
+                            {/* CHARTS */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
+                        </Routes>
                     </div>
                 </div>
             </BrowserRouter>
